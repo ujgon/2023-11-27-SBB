c44be2f36045200b7bb085909a1a2bb2c5d79ce6
Yoo <ujgon@naver.com>
2023-11-28 03:22:04 +0900

    2_04

---
+++ src/main/java/com/ll/sbb202311/Answer.java
@@ -0,0 +1,27 @@
+package com.ll.sbb202311;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+
+@Getter
+@Setter
+@Entity
+public class Answer {
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(columnDefinition = "TEXT")
+    private String content;
+
+    private LocalDateTime createDate;
+
+    @ManyToOne
+    private Question question;
+
+}
