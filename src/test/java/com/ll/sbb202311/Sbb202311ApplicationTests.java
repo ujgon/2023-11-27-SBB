c27c6e67c6001f534aab39b9d56ee6bf897099b0
Yoo <ujgon@naver.com>
2023-11-27 22:02:47 +0900

    project init

---
+++ src/test/java/com/ll/sbb202311/Sbb202311ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ll.sbb202311;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sbb202311ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
